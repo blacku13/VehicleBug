f69321497479ff6840814f211fa171db3e83d8ee
Aditya010 <rathiaditya53@gmail.com>
2019-03-08 11:50:13 +0530

    Android App updated

--- Andoid App/VehicleBug-master/VehicleBug-master/app/src/main/java/crop/iisc/project/croppestdetector/Utils.java
+++ Andoid App/VehicleBug-master/VehicleBug-master/app/src/main/java/crop/iisc/project/croppestdetector/Utils.java
@@ -4,7 +4,7 @@ package crop.iisc.project.croppestdetector;
  * Created by Sachin on 1/13/2016.
  */
 public class Utils {
-    public static final String urlUpload = "http://35.200.167.133:5000/processjson";
+    public static final String urlUpload = "http://35.200.151.98:5000/processjson";
     public static final int REQCODE = 100;
     public static final String image = "image";
     public static final String imageName = "name";
